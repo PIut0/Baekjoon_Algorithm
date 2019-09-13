eda258eb0567e0f2f6ec8b26dac3a5b21080c98a
Plut0 <slimdoks@gmail.com>
2019-09-14 02:31:42 +0900

    Test

---
+++ 14912.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning (disable : 4996)
+
+int main() {
+  int n,t,cnt;
+  cnt = 0;
+  scanf("%d %d",&n, &t);
+  for(int i=1; i<=n; i++){
+    for(int j=i; j>0; j=j/10){
+      if(j%10 == t)
+        cnt++;
+    }
+  }
+  printf("%d",cnt);
+}
