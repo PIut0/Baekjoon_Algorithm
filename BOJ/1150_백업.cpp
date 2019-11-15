d21c86868c8e099861ef2a9312a26d8eb891204c
plut0 <slimdoks@gmail.com>
2019-11-15 21:06:13 +0900

    PC_2019_11_15

---
+++ BOJ/1150_백업.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#define mp make_pair
+#define pb push_back
+using namespace std;
+
+
+priority_queue<pair<int, pair<int, int> > , vector<pair<int, pair<int, int> > >, greater<pair<int, pair<int, int> > > > pq;
+int arr[100003];
+int conn[100003][2];
+int check[100003]={0,};
+map <int, int> checkmap;
+
+int main(){
+    int n,k,x=-1234567890;
+    scanf("%d %d",&n,&k);
+    for(int i=1;i<=n;i++){\
+        scanf("%d",&arr[i]);
+        int o = arr[i];
+        arr[i] = o-x;
+        x = o;
+        checkmap[i] = i;
+        pq.push(mp(arr[i],mp(i-1,i)));
+        // printf("%d\n", pq.top().first);
+    }
+
+    arr[0] = 1234567890;
+    arr[n+1] = 1234567890;
+
+    x = 0;
+    while(k--){
+        int min;
+        int idxl,idxr;
+        min = pq.top().first;
+        idxl = pq.top().second.first;
+        idxr = pq.top().second.second;
+        // printf("%d\n",min);
+
+        pq.pop();
+        if(check[idxl] == 0 && check[idxr] == 0){
+            check[idxl] = 1;
+            check[idxr] = 1;
+            int sum;
+            sum = arr[idxl] + arr[idxr+1]- arr[idxr];
+            pq.push(mp(sum,mp(idxl-1,idxr+1)));
+            x += min;
+            printf("%d\n",min);
+        }
+        else{
+            k++;
+        }
+    }
+    printf("%d",x);
+}
--- test.cpp
+++ test.cpp
@@ -1,143 +1,32 @@
-//프로그램 개요
-//작성자:
-//작성일: 
 #include <stdio.h>
-#include <math.h>
-#define PI 3.14
+#include <stdlib.h>
 
-//사각형의 넓이를 구하는 함수 
-double Area_of_Rectangle(double x, double y){
-   return x*y;
-}
+typedef struct node{
+  int info;
+  struct node *link;
+} Node;
 
-//삼각형의 넓이를 구하는 함수
-double Area_of_Triangle(double x, double y, double z){
-   double s=0;
-   s = (x+y+z)/2;
-   return sqrt(s*(s-x)*(s-y)*(s-z));
-}
+int main(void) {
+  Node *head = NULL;
 
-//원의 넓이를 구하는 함수
-double Area_of_Circle(double x){
-   return x*x*PI;
-} 
+  head = (Node *)malloc(sizeof(Node));
+  head -> info = 0;
+  head -> link = NULL;
+  Node *nodes[5];
+  nodes[0] = (Node *)malloc(sizeof(Node));
+  head -> link = nodes[0];
+  for(int i=0;i<4;i++){
+     nodes[i+1] = (Node *)malloc(sizeof(Node));
+     scanf("%d",&nodes[i]->info);
+     nodes[i] -> link = nodes[i+1];
+  }
+  scanf("%d",&nodes[4]->info);
+  nodes[4] -> link = NULL;
+  Node *cur = head -> link -> link -> link;
+  while(cur != NULL){
+     printf("%d\n",cur->info);
+     cur = cur->link;
+  }
 
-//평행사변형의 넓이를 구하는 함수
-double Area_of_Parallelogram(double x, double y){
-   return x*y;
-} 
-
-//최댓값을 구하는 함수
-double maxi(double x, double y, double z){
-   double max=0;
-   if(x>=y){
-      max = x;
-      if(max>=z){
-         max = max;
-      }
-      else if(max<z){
-         max = z;
-      }
-   }
-   else if(x<y){
-      max = y;
-      if(max>=z){
-         max = max;
-      }
-      else if(max<z){
-         max = z;
-      }
-   }
-   return max;
-} 
-
-//main 함수 
-int main(){
-   char menu;
-   double sum=0, max_s;
-   double x, y, z;
-   
-   //'q'가 나오기 전까지 계속 반복 
-   while(menu!='q'){
-      // 초기메뉴 공지 및 메뉴 입력 
-      printf("1:사각형의 넓이 구하기\n2:삼각형의 넓이 구하기\n3:원의 넓이 구하기\n4:평행사변형의 넓이 구하기\nq:프로그램 종료\n원하는 메뉴를 선택하시오: ");
-      scanf("%c", &menu); 
-      
-      //1번 메뉴를 선택했을 때 
-      if(menu == '1'){
-         printf("가로와 세로의 길이를 입력하시오: ");
-         scanf("%lf %lf",&x,&y);
-         if(x==0 || y==0){
-            printf("0은 유효하지 않은 입력 값입니다.\n");
-            scanf("%lf%lf", &x, &y);
-         }
-         else if(x<0 || y<0){
-            printf("음수는 유효하지 않은 입력 값입니다.\n");
-            scanf("%lf%lf", &x, &y);
-          }
-         sum = Area_of_Rectangle(x,y);
-         printf("사각형의 넓이는 %lf입니다.\n", sum);
-      }
-      
-      //2번 메뉴를 선택했을 때 
-      else if(menu == '2'){
-         printf("세변의 길이를 입력하시오: ");
-         scanf("%lf%lf%lf", &x, &y, &z);
-         max_s = maxi(x, y, z);
-         if(x==0 || y==0 || z==0){
-            printf("0은 유효하지 않은 입력 값입니다.\n");
-            scanf("%lf%lf%lf", &x, &y, &z);
-         }
-         else if(x<0 || y<0 || z<0){
-            printf("음수는 유효하지 않은 입력 값입니다.\n");
-            scanf("%lf%lf%lf", &x, &y, &z);
-         }
-         else if(max_s>x+y+z-max_s){
-            printf("가장 긴 변의 크기가 나머지 두변을 더한값 보다 큽니다.\n");
-            scanf("%lf%lf%lf", &x, &y, &z);
-         }
-         sum = Area_of_Triangle(x, y, z);
-         printf("삼각형의 넓이는 %lf입니다.\n", sum);
-      }
-      
-      //3번 메뉴를 선택했을 때 
-      else if(menu == '3'){
-         printf("반지름을 입력하시오: ");
-         scanf("%lf", &x);
-         if(x==0){
-            printf("0은 유효하지 않은 입력 값입니다.\n");
-            scanf("%lf", &x);
-         }
-         else if(x<0){
-            printf("음수는 유효하지 않은 입력 값입니다.\n");
-            scanf("%lf", &x);
-         }
-         sum = Area_of_Circle(x);
-         printf("원의 넓이는 %lf입니다.\n", sum); 
-      }
-      
-      //4번 메뉴를 선택했을 때 
-      else if(menu == '4'){
-         printf("밑변과 높이를 입력하시오: ");
-         scanf("%lf%lf", &x, &y);
-         if(x==0 || y==0){
-            printf("0은 유효하지 않은 입력 값입니다.\n");
-            scanf("%lf%lf", &x, &y);
-         }
-         else if(x<0 || y<0){
-            printf("음수는 유효하지 않은 입력 값입니다.\n");
-            scanf("%lf%lf", &x, &y);
-         }
-         sum = Area_of_Parallelogram(x,y);
-         printf("평행사변형의 넓이는 %lf입니다.\n", sum);
-      }
-      
-      scanf("%c");
-      //초기 메뉴에 없는 메뉴를 선택했을 때 
-      if(menu != '1' && menu != '2' && menu != '3' && menu != '4' && menu != 'q'){
-         printf("선택할 수 없는 메뉴입니다.\n %c\n",menu);
-      }
-
-   }
-
+  return 0;
 }
